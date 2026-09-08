cef4095613afc2da241613b979d67834b03aec50
thuymmai <thuymmai11@gmail.com>
2026-09-07 20:11:29 -0500

    Initial commit

---
+++ src/main/java/com/project/pokemon_system/PokemonSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.pokemon_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/pokemon_system/PokemonSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pokemon_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
